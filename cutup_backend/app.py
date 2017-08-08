8de581df082acb6cfd574f700a95f8953387ace3
Larry Hernandez <larryhernanjr@gmail.com>
2017-08-08 18:00:42 -0400

    return json with scrambled headlines

--- cutup_backend/app.py
+++ cutup_backend/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import random
 import os
+import datetime
+import textwrap
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -10,10 +13,99 @@ db = SQLAlchemy(app)
 from models import Result
 from services.scraper import Scraper
 
-@app.route("/")
-def index():
+@app.route("/tell-the-tale")
+def tell_the_tale():
+    date = str(datetime.date.today())
+    collection = []
+    the_tale = {}
+    counter = 0
+    for instance in Result.query.filter_by(date=date).all():
+        ten_words = textwrap.wrap(instance.titles, 50)
+        collection += ten_words
+    collection = random.sample(collection, len(collection))
+    for i in range(0, len(collection) - 4, 23):
+        the_tale[counter] = "" + collection[i] + " " + collection[i+3]
+        counter += 1
+    return jsonify(the_tale)
+
+@app.route("/results", methods=["POST"])
+def show_results():
+    date = str(datetime.date.today())
+    results = {}
+    for instance in Result.query.filter_by(date=date).all():
+        results[instance.source] = {
+            'date': instance.date,
+            'titles': instance.titles
+        }
+    return jsonify(results)
+
+@app.route("/api/v1/bbc", methods=["POST"])
+def bbcFetch():
+    script = Scraper().scrape('bbc')
+    scrape_data = {
+        'source': "bbc",
+        'date': datetime.date.today(),
+        'titles': " ".join(script)
+    }
+    try:
+        result = Result(
+            source = scrape_data['source'],
+            date = scrape_data['date'],
+            titles = scrape_data['titles']
+        )
+        print(result)
+        db.session.add(result)
+        db.session.commit()
+    except:
+        return "Unable to connect to Database"
+    return jsonify(scrape_data)
+
+@app.route("/api/v1/nyt", methods=["POST"])
+def nytFetch():
+    script = Scraper().scrape('nyt')
+    result = Result(
+        source = "nyt",
+        date = datetime.date.today(),
+        titles = " ".join(script)
+    )
+    print(script)
+    db.session.add(result)
+    db.session.commit()
+
+@app.route("/api/v1/fox", methods=["POST"])
+def foxFetch():
+    script = Scraper().scrape('fox')
+    result = Result(
+        source = "fox",
+        date = datetime.date.today(),
+        titles = " ".join(script)
+    )
+    db.session.add(result)
+    db.session.commit()
+    return "202"
+
+@app.route("/api/v1/alj", methods=["POST"])
+def aljFetch():
     script = Scraper().scrape('alj')
-    return script
+    result = Result(
+        source = "alj",
+        date = datetime.date.today(),
+        titles = " ".join(script)
+    )
+    db.session.add(result)
+    db.session.commit()
+    return "202"
+
+@app.route("/api/v1/cnn", methods=["POST"])
+def cnnFetch():
+    script = Scraper().scrape('cnn')
+    result = Result(
+        source = "cnn",
+        date = datetime.date.today(),
+        titles = " ".join(script)
+    )
+    db.session.add(result)
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run()
--- cutup_backend/services/scraper.py
+++ cutup_backend/services/scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib
+import re
 
 class Scraper(object):
     def bbcScrape(self, url):
@@ -8,9 +9,10 @@ class Scraper(object):
         soup = BeautifulSoup(site_text, 'html.parser')
         titles = soup.find_all('h3')
         for title in titles:
-            collection.append(title.get_text())
-        joined_collection = ' '.join(collection)
-        return joined_collection
+            title_text = title.get_text().lower()
+            clean_title = re.sub(r'[\t\n]', '', title_text)
+            collection.append(clean_title)
+        return collection
 
     def nytScrape(self, url):
         collection = []
@@ -18,9 +20,11 @@ class Scraper(object):
         soup = BeautifulSoup(site_text, 'html.parser')
         titles = soup.find_all('h2', 'headline')
         for title in titles:
-            collection.append(title.get_text())
-        joined_collection = ' '.join(collection)
-        return joined_collection
+            title_text = title.get_text().lower()
+            clean_title = re.sub(r'[\t\n]', '', title_text)
+            spaced_title = re.sub(r'\s+', ' ', clean_title)
+            collection.append(spaced_title)
+        return collection
 
     def aljScrape(self, url):
         collection = []
@@ -28,9 +32,33 @@ class Scraper(object):
         soup = BeautifulSoup(site_text, 'html.parser')
         titles = soup.find_all(['h1', 'h2', 'h3', 'h4'])
         for title in titles:
-            collection.append(title.get_text())
-        joined_collection = ' '.join(collection)
-        return joined_collection
+            title_text = title.get_text().lower()
+            clean_title = re.sub(r'[\t\n]', '', title_text)
+            collection.append(clean_title)
+        return collection
+
+    def foxScrape(self, url):
+        collection = []
+        site_text = urllib.urlopen(url).read()
+        soup = BeautifulSoup(site_text, 'html.parser')
+        titles = soup.find_all(['h3', 'h4'])
+        for title in titles:
+            title_text = title.get_text().lower()
+            clean_title = re.sub(r'[\t\n]', '', title_text)
+            collection.append(clean_title)
+        return collection
+
+    def cnnScrape(self, url):
+        collection = []
+        site_text = urllib.urlopen(url).read()
+        soup = BeautifulSoup(site_text, 'html5lib')
+        titles = soup.find_all('h3')
+        print(titles)
+        for title in titles:
+            title_text = title.get_text().lower()
+            clean_title = re.sub(r'[\t\n]', '', title_text)
+            collection.append(clean_title)
+        return collection
 
     def scrape(self, string):
         if string == "bbc":
@@ -42,7 +70,9 @@ class Scraper(object):
         elif string == "alj":
             url = 'http://www.aljazeera.com/'
             return self.aljScrape(url)
-        elif string == "fnews":
+        elif string == "fox":
             url = 'http://www.foxnews.com/'
+            return self.foxScrape(url)
         elif string == "cnn":
-            url = 'http://www.cnn.com/'
+            url = 'http://www.cnn.com/world'
+            return self.cnnScrape(url)
